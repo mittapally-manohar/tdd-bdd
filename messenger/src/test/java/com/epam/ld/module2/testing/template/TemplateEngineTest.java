d22e244605729622b3da76bc9d6f25af49e33553
Manohar_Mittapally <mittapallymanohar194@gmail.com>
2025-03-03 23:55:19 +0530

    red - The system replaces variable placeholders like #{subject} from a template with values provided at runtime.

--- messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,4 +1,20 @@
 package com.epam.ld.module2.testing.template;
 
+import com.epam.ld.module2.testing.Client;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class TemplateEngineTest {
+
+    @Test
+    public void testSystemReplacePlaceholdersWithValuesProvidedAtRuntime() {
+        Template template = new Template();
+        Client client = new Client();
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        String message = templateEngine.generateMessage(template, client);
+
+        fail();
+    }
 }
